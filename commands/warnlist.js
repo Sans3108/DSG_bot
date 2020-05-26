b9737319417cf3c548049089d5f9a2063d27d1ad
Glitch (dsg-boat) <none>
2020-05-26 11:40:21 +0000

    Poggers warnlist now :o

--- commands/warnlist.js
+++ commands/warnlist.js
@@ -6,7 +6,7 @@ module.exports = {
   name: "warnlist",
   description: "Lists all the existing warnings for everyone!",
   group: "admin",
-  cooldown: 300,
+  cooldown: 180,
   aliases: ["listwarns"],
   guildOnly: true,
   execute: async (message, args, bot, config, command, aargs) => {
@@ -16,9 +16,7 @@ module.exports = {
     ) {
       await message.guild
         .fetchMembers()
-        .then(() =>
-          console.log("Fetched all members from " + message.guild.name)
-        )
+        .then(() => console.log("Fetched all members from " + message.guild.name))
         .catch(e => {
           console.log(e.stack);
           message.channel.send(
@@ -26,52 +24,105 @@ module.exports = {
               `\nPlease contact <@${config.ownerID}> or any staff and show them this error.`
           );
         });
-      
+    
       let icon = message.guild.iconURL.slice(0, -3) + 'gif';
-      
-      let emb1 = new Discord.RichEmbed()
-        .setColor(config.color.blue)
-        .setThumbnail(icon)
-        .setTitle("Warn list for **" + message.guild.name + "**:")
-        .setDescription('It will probably take a bit to fetch everything...\nI\'ll let you know when I\'m done!')
-        
-      message.channel.send(emb1);
-      
-      function compare(a, b) {
-        return Number(a.wid) - Number(b.wid);
-      }
-      
-      let all = db.all().filter(i => i.ID !== 'CONFIG').map(i => i.ID);
-      let chunks = _.chunk(all, 25);
-        
-      await chunks.forEach(async i => {
-        let emb = new Discord.RichEmbed()
-          .setColor(config.color.blue);
-        
-        await i.forEach(item => {
-          let CASE = db.fetch(item);
-        
-          let USER = message.guild.members.get(item);
 
+      let all = db.all().filter(i => i.ID !== "CONFIG").map(i => i.ID);
+    
+      let filtered = [];
+    
+      await all.forEach(item => {
+        let member = message.guild.members.get(item);
+      
+        if(member) {
+          filtered.push(item);
+        }
+      });
+    
+      let chunks = _.chunk(filtered, 5);
+      let pages = [];
+      let totalPages = chunks.length;
+      let count = 0;
+    
+      await chunks.forEach(async chunk => {
+        let emb = new Discord.RichEmbed()
+          .setColor(config.color.blue)
+          .setThumbnail(icon)
+          .setTitle("Warn list for **" + message.guild.name + "**:");
+      
+        await chunk.forEach(item => {
+          let CASE = db.fetch(item);
+          let USER = message.guild.members.get(item);
+        
+          function compare(a, b) {
+            return Number(a.wid) - Number(b.wid);
+          }
+        
           let list = CASE.warnings.sort(compare).map(w =>`\`Case ${w.wid}:\` **${w.reason}**\n(By: <@${w.warnedBy}> in <#${w.where}> at ${w.when})`);
         
           if (USER && list[0]) {        
             emb.addField(`Tag: ***${USER.user.tag}***   ID: ***${item}***`, list);
           }
-        })
-        
-        await message.channel.send(emb);
+        });
+        count = count + 1;
+        emb.setFooter(`Page: ${count}/${totalPages}`);
+        pages.push({ count: count, emb: emb});
       });
+
+      let emb1 = new Discord.RichEmbed()
+        .setColor(config.color.blue)
+        .setThumbnail(icon)
+        .setTitle("Warn list for **" + message.guild.name + "**:")
+        .setDescription('Loading...')
+        .setFooter(`Page: ?/?`);
+    
+      let msg = await message.channel.send(emb1);
+    
+      let currentPage = 1;
+    
+      await msg.edit('', { embed: pages[0].emb });
       
-      let emb2 = new Discord.RichEmbed()
-        .setColor(config.color.green)
-        .setDescription('Done fetching all the warnings.');
-      
-      await message.channel.send(emb2);
-      if(bot.f.chance(5)) {
-        message.channel.send('Damn that\'s a lotta warns :v');
+      if (bot.f.chance(5)) {
+        message.channel.send("Damn that's a lotta warns :v");
         // Easter eggs for the win baby :)
       }
+    
+      await msg.react('⬅');
+      await msg.react('⏹');
+      await msg.react('➡');
+    
+      const filter = (reaction, user) => {
+	      return reaction.emoji.name === '⬅' || reaction.emoji.name === '⏹' || reaction.emoji.name === '➡' && user.id !== bot.user.id && !user.bot;
+      };
+    
+      const collector = msg.createReactionCollector(filter, { time: 120000 });
+
+      collector.on('collect', reaction => {
+	    if(reaction.emoji.name === '⏹') {
+        collector.stop();
+      } else if(reaction.emoji.name === '➡') {
+        if(currentPage < totalPages) {
+          currentPage = currentPage + 1;
+          
+          let page = pages.find(page => page.count === currentPage);
+      
+          msg.edit('', { embed: page.emb });
+        }
+      } else if(reaction.emoji.name === '⬅') {
+        if(currentPage > 1) {
+          currentPage = currentPage - 1;
+        
+          let page = pages.find(page => page.count === currentPage);
+      
+          msg.edit('', { embed: page.emb });
+        }
+      }
+    });
+
+    collector.on('end', collected => {
+	    msg.clearReactions();
+    });
+      
     } else return;
   }
 };
