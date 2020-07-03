b82d009760f9dec7fac7097abd59006f838c0841
Sans3108 <dragnerobertleonard13@gmail.com>
2020-07-03 17:43:53 +0300

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -7,6 +7,7 @@ module.exports = {
   aliases: ['np', 'nowplaying'],
   cooldown: 1,
   guildOnly: true,
+  disabled: true,
   execute: async (message, args, bot, config, command, aargs) => {
     let queue = bot.queue.get(message.guild.id);
     
