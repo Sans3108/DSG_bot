936ccdea0c2f47b078832fcf2838ff85b33c72a8
Sans3108 <dragnerobertleonard13@gmail.com>
2020-07-03 17:44:21 +0300

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -7,6 +7,7 @@ module.exports = {
   group: "music",
   cooldown: 2,
   guildOnly: true,
+  disabled: true,
   execute: async (message, args, bot, config, command, aargs) => {
     let queue = bot.queue.get(message.guild.id);
 
